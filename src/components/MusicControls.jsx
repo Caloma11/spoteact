0c2fdb907a23aa7ef6014886b3a70b8afc7300d0
gianluca <perronegian@hotmail.com>
2020-05-27 12:38:15 +0800

    sidebar styling

--- src/components/MusicControls.jsx
+++ src/components/MusicControls.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MusicControls = () => {
     return(
         <div className="musicControls">
-            Caju azedo
+           {'<< |> >>'}
         </div>
     )
 }
